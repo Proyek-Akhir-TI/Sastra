a10812008aacb3cee0ebe9c7eb4f4018460d3e39
Dhimas Panji Sastra <dhimaspanji66@gmail.com>
2020-03-10 19:51:10 +0700

    Fresh Android

---
+++ Android1/app/src/main/java/sastra/panji/dhimas/cov19/MainActivity.java
@@ -0,0 +1,14 @@
+package sastra.panji.dhimas.cov19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
