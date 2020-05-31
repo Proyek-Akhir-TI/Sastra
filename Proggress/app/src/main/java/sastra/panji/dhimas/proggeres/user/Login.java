1b0497b88365aff4b28f496fd208ff6fba13f330
Dhimas Panji Sastra <tumbalig001@gmail.com>
2020-06-01 03:52:56 +0700

    Penambahan Layout Login

---
+++ Proggress/app/src/main/java/sastra/panji/dhimas/proggeres/user/Login.java
@@ -0,0 +1,16 @@
+package sastra.panji.dhimas.proggeres.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sastra.panji.dhimas.proggeres.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
