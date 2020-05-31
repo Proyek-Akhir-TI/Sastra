5f0c9a33c961d41d9a8795919a53fdaba60caecc
Dhimas Panji Sastra <tumbalig001@gmail.com>
2020-06-01 05:00:12 +0700

    Menambah Tampilan Daftar

---
+++ Proggress/app/src/main/java/sastra/panji/dhimas/proggeres/user/Daftar.java
@@ -0,0 +1,16 @@
+package sastra.panji.dhimas.proggeres.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sastra.panji.dhimas.proggeres.R;
+
+public class Daftar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar);
+    }
+}
