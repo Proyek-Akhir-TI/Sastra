9a74a6f0f744aecfaed1be7b11e61233cb90f1fe
Dhimas Panji Sastra <tumbalig001@gmail.com>
2020-07-12 12:38:49 +0700

    menambahkan activity atur notifikasi

---
+++ Proggress/app/src/main/java/sastra/panji/dhimas/proggeres/menu/AturNotif.java
@@ -0,0 +1,16 @@
+package sastra.panji.dhimas.proggeres.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sastra.panji.dhimas.proggeres.R;
+
+public class AturNotif extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atur_notif);
+    }
+}
