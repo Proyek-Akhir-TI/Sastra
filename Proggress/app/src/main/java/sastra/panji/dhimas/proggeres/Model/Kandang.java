59e2368042fd425579611abf5423e1a7aa802db1
Dhimas Panji Sastra <tumbalig001@gmail.com>
2020-08-29 21:06:04 +0700

    penambahan model no_telpon

--- Proggress/app/src/main/java/sastra/panji/dhimas/proggeres/Model/Kandang.java
+++ Proggress/app/src/main/java/sastra/panji/dhimas/proggeres/Model/Kandang.java
@@ -1,11 +1,85 @@
 package sastra.panji.dhimas.proggeres.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Kandang {
+public class Kandang implements Parcelable {
+    public static final Creator<Kandang> CREATOR = new Creator<Kandang>() {
+        @Override
+        public Kandang createFromParcel(Parcel source) {
+            return new Kandang(source);
+        }
+
+        @Override
+        public Kandang[] newArray(int size) {
+            return new Kandang[size];
+        }
+    };
+    int isActive;
     private String name;
-    private long lat,lang;
-private List<String> kandang;
+    private String url;
+    private String id;
+    private String telpon;
+    private String foto;
+    private String latitude, longitude;
+    private List<String> kandang;
+
+    public Kandang() {
+    }
+
+    protected Kandang(Parcel in) {
+        this.name = in.readString();
+        this.url = in.readString();
+        this.id = in.readString();
+        this.telpon = in.readString();
+        this.foto = in.readString();
+        this.isActive = in.readInt();
+        this.latitude = in.readString();
+        this.longitude = in.readString();
+        this.kandang = in.createStringArrayList();
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
 
     public List<String> getKandang() {
         return kandang;
@@ -23,19 +97,37 @@ private List<String> kandang;
         this.name = name;
     }
 
-    public long getLat() {
-        return lat;
+    public String getLatitude() {
+        return latitude;
     }
 
-    public void setLat(long lat) {
-        this.lat = lat;
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
     }
 
-    public long getLang() {
-        return lang;
+    public String getLongitude() {
+        return longitude;
     }
 
-    public void setLang(long lang) {
-        this.lang = lang;
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.url);
+        dest.writeString(this.id);
+        dest.writeString(this.telpon);
+        dest.writeString(this.foto);
+        dest.writeInt(this.isActive);
+        dest.writeString(this.latitude);
+        dest.writeString(this.longitude);
+        dest.writeStringList(this.kandang);
     }
 }
